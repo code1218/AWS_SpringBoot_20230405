8e889d0711b74fcdfde04d87ccdaccee40109c59
code1218 <skjil1218@gmail.com>
2023-04-10 17:48:53 +0900

    select

---
+++ src/main/java/com/web/study/domain/entity/Student.java
@@ -0,0 +1,25 @@
+package com.web.study.domain.entity;
+
+import java.time.LocalDate;
+
+import com.web.study.dto.response.StudentRespDto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+//@Alias("S1")
+public class Student {
+	private int id;
+	private String name;
+	private LocalDate birth_date;
+	
+	public StudentRespDto toDto() {
+		return StudentRespDto.builder()
+				.id(id)
+				.name(name)
+				.birthDate(birth_date)
+				.build();
+	}
+}
